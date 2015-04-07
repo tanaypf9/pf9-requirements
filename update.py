121c33d71032b4d51e062bd73fa51e4a177d301f
Sean M. Collins <sean@coreitpro.com>
2015-04-07 07:28:18 -0400

    Check for the url attribute before accessing
    Some dependencies, like aioeventlet appear to not have a url when
    parsed.
    
    http://paste.openstack.org/show/199334/
    
    Change-Id: I2d3e4ec69e48e5a574eeb430e052bd500d815601

--- update.py
+++ update.py
@@ -98,7 +98,7 @@ def _parse_pip(pip):
     install_require = req.InstallRequirement.from_line(pip)
     if install_require.editable:
         return pip
-    elif install_require.url:
+    elif hasattr(install_require, 'url') and install_require.url:
         return pip
     else:
         return install_require.req.key
