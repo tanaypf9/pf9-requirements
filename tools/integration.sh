ae2d5d5700eb03e60ed01e95f25713cb7b56c0ae
Robert Collins <rbtcollins@hp.com>
2015-06-24 16:09:13 +1200

    Test that changes to requirements are freezable.
    If we can't generate a new constraints file, don't let changes in.
    
    Change-Id: If5aa15bb9ca75db0b8ef89a13c9b4bbf8d45cae2

--- tools/integration.sh
+++ tools/integration.sh
@@ -77,6 +77,12 @@ install_all_of_gr
 $tmpdir/all_requirements/bin/pip install $REPODIR/requirements
 UPDATE="$tmpdir/all_requirements/bin/update-requirements"
 
+# Check that we can generate an upper-requirements.txt file with the change
+# that is being proposed.
+$tmpdir/all_requirements/bin/generate-constraints -p /usr/bin/python2.7 \
+    -p /usr/bin/python3.4 -b $REPODIR/requirements/blacklist.txt \
+    -r $REPODIR/requirements/global-requirements.txt
+
 for PROJECT in $PROJECTS ; do
     SHORT_PROJECT=$(basename $PROJECT)
     if ! grep 'pbr' $REPODIR/$SHORT_PROJECT/setup.py >/dev/null 2>&1
