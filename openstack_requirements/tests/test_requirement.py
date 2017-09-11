fd05bb4fc47edd9186d3dcecd5b32589f4ea3adc
Dirk Mueller <dirk@dmllr.de>
2017-09-11 15:46:34 -0600

    Add policy check on global-requirements.txt entries
    We want to ensure that every entry in global-requirements has
    at least a minimum version specified, and if there is an exclude,
    the exclude is not outside the lower bounds.
    
    Change-Id: Ie12d99d3317bd4c81de9a47a43fa99345b2b9664

--- openstack_requirements/tests/test_requirement.py
+++ openstack_requirements/tests/test_requirement.py
@@ -194,3 +194,25 @@ class TestToDict(testtools.TestCase):
         req = requirement.Requirement('Foo_bar', '', '', '', '')
         self.assertEqual(
             {'foo-bar': [(req, '')]}, requirement.to_dict([(req, '')]))
+
+
+class TestReqPolicy(testtools.TestCase):
+
+    def test_requirements_policy_pass(self):
+        content = textwrap.dedent("""\
+            cffi>=1.1.1,!=1.1.2
+            other>=1.1.1
+            """)
+        reqs = requirement.parse(content)
+        policy_check = [x for x in requirement.check_reqs_bounds_policy(reqs)]
+        self.assertEqual(len(policy_check), 0)
+
+    def test_requirements_policy_fail(self):
+        content = textwrap.dedent("""\
+            cffi>=1.1.1,!=1.1.0
+            other>=1,>=2,!=1.1.0
+            no_lower_bound
+            """)
+        reqs = requirement.parse(content)
+        policy_check = [x for x in requirement.check_reqs_bounds_policy(reqs)]
+        self.assertEqual(len(policy_check), 3)
